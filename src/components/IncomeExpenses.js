44a2b8bd62c56ea79b5bca932e31609340a7f284
Ecelik-git <elvanchelik@gmail.com>
2021-06-24 23:32:06 -0500

    june24

---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const IncomeExpenses = () => {
+    return (
+        <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+ 
+        </div>
+        <div>
+          <h4>Expense</h4>
+
+        </div>
+      </div>
+    )
+}
+
+export default IncomeExpenses;
+
