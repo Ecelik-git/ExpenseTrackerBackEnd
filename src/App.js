bae1738981af770c514b6466c79748b1ef5f10d2
Ecelik-git <elvanchelik@gmail.com>
2021-06-25 23:29:40 -0500

    june25th

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {Balance} from "./components/Balance";
 import {IncomeExpenses} from "./components/IncomeExpenses";
 import {TransactionList} from "./components/TransactionList";
 import {AddTransaction} from "./components/AddTransaction";
+import { GlobalProvider } from './context/GlobalState';
 
 
 export const App = () => {
     return (
-        <div>
+        <GlobalProvider>
             <Header />
             <div className="container">
                 <Balance />
@@ -17,8 +18,15 @@ export const App = () => {
                 <TransactionList /> 
                 <AddTransaction />
             </div>
-        </div>
+        </GlobalProvider>
     )
 }
 
 export default App;
+
+////1. npm install react-scripts start  : THIS COMMENT IS USED TO ADD THE DEPENDENCIESE IN THE REACT APP
+//WHEN EVER WE START A NEW PROJECT, WE DO THIS ONLY ONCE TO ADD REACT LIBRARIES
+//OPEN TERMINAL TO RUN THIS 
+//2. npm install : Double check the intallation
+//3. npm start : OPENS THE REACT PAGE
+//npm => node package manager
