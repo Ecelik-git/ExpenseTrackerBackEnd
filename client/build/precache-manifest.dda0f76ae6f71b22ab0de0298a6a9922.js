cfff9b506e03a84d532cf0a23277f78e509ff477
Ecelik-git <elvanchelik@gmail.com>
2021-07-02 23:09:02 -0500

    july2ndFinal

---
+++ client/build/precache-manifest.dda0f76ae6f71b22ab0de0298a6a9922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe60305b1ce1702f79ab9360f57bc597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a0532ce174e399f01d2",
+    "url": "/static/css/main.9f1dc771.chunk.css"
+  },
+  {
+    "revision": "c9371f164ae9df595676",
+    "url": "/static/js/2.6ec30252.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.6ec30252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0532ce174e399f01d2",
+    "url": "/static/js/main.5e258549.chunk.js"
+  },
+  {
+    "revision": "6d6d2bc334df50e113de",
+    "url": "/static/js/runtime-main.65646eda.js"
+  }
+]);
--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -36,18 +36,40 @@ export const GlobalProvider =({children}) => {
     }
 
 
-    function addTransaction(transaction){
-        dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
-        });
+    async function addTransaction(transaction){
+        const config = {
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        }
+        try{
+            const res= await axios.post('/api/v1/transactions', transaction, config);
+            dispatch({
+                type: 'ADD_TRANSACTION',
+                payload: res.data.data
+            });
+        }catch (err){
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            });
+        }
+        
     }
 
-    function deleteTransaction(id){
-        dispatch({
+   async function deleteTransaction(id){
+       try{
+           await axios.delete(`/api/v1/transactions/${id}`);
+           dispatch({
             type: 'DELETE_TRANSACTION',
             payload: id
         });
+       }catch (err){
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: err.response.data.error
+            });
+        }
     }
 
 
